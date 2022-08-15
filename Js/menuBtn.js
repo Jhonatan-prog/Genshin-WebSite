64ce35fc8e8522c122fbcbd145d87c843084c73e
Jhonatan-prog <110551929+Jhonatan-prog@users.noreply.github.com>
2022-08-15 11:24:17 -0500

    Change name Js

--- Js/menuBtn.js
+++ Js/menuBtn.js
@@ -1,6 +1,5 @@
 const openMenuBtn = document.querySelector('.Open-Menu');
 const closeMenuBtn = document.querySelector('Close-Menu');
-const MenuBody = document.querySelector('container-navbar');
 
 openMenuBtn.addEventListener('click', () => {
     MenuBody.classList.toggle('Open')
